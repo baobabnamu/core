b609bad5064178a3ce815e334a744856e7487e18
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-01 20:07:57 +0900

    add: 섹션2 - 회원 도메인 구현 및 테스트

---
+++ src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+
+// 아래와 같이 자바 프로그램을 직접 작성하는 것은 좋은 유닛 테스트 방식이 아니다. 유닛 테스트 프레임워크인 JUnit 으로 테스트 하자!
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member : " + member.getName());
+        System.out.println("find Member : " + findMember.getName());
+    }
+}
