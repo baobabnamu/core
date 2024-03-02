260e6250e13cbb08a7951f39a553672285c4414a
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-03 00:34:23 +0900

    add: DIP, SRP 원칙 보장을 위해 AppConfig 도입 (모든 서비스 DI 도입)

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -8,7 +8,7 @@ import hello.core.member.MemberServiceImpl;
 // 아래와 같이 자바 프로그램을 직접 작성하는 것은 좋은 유닛 테스트 방식이 아니다. 유닛 테스트 프레임워크인 JUnit 으로 테스트 하자!
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        MemberService memberService = new AppConfig().memberService();
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.AppConfig;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Grade;
@@ -7,11 +8,19 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
