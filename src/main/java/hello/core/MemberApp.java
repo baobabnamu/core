fc63da63ec34624344a300ac07c76ef5c6a2af53
JaeHwan Kim <79235021+baobabnamu@users.noreply.github.com>
2024-03-03 17:04:56 +0900

    TEST: 빈 등록 및 빈 조회 테스트

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,11 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 아래와 같이 자바 프로그램을 직접 작성하는 것은 좋은 유닛 테스트 방식이 아니다. 유닛 테스트 프레임워크인 JUnit 으로 테스트 하자!
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new AppConfig().memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
